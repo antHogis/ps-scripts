bceceeeb5fee160d0d0c0f4c3e8dcf9db45b6ad6
Anton Hoglund <antonjhoglund@hotmail.com>
2019-01-18 13:53:23 +0200

    Update cpanex comments

--- cpanex.ps1
+++ cpanex.ps1
@@ -1,13 +1,18 @@
+# This script copies source files from an Android project to an exercise folder.
+# Files from a directory called src in the current directory is copied to a 
+# specified directory a step back in the directory tree. The specified directory
+# will be created if it does not exist.
+
 param (
-    #Uses parameter as destination folder, if arguments not present, default value is prompting input
+    # Destination folder, if arguments not present, default value is prompting input
     [string]$exerciseFolder = (Read-Host -Prompt 'Input destination folder name'),
+    # Option for forcing file copying. Files will be overwritten if this switch is
+    # used, otherwise not.
     [switch]$F = $false
 )
 
 $exercisePath = -join((Get-Item -Path "..\").FullName,"\",$exerciseFolder,"\src\")
 $srcPath = (Get-Item -Path ".\src\main").FullName
-
-#tubular dude
 
 New-Item -ErrorAction Ignore -ItemType Directory -Path $exercisePath
 
